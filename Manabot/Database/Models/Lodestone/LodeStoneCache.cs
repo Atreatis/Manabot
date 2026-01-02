3752e07b4c5ff4fa332c8d950dfe70a0e430be23
Tech Potion <dmontijn@potionlab.net>
2026-01-02 12:54:26 +0100

    Remove lodetone models from Database folders

--- Manabot/Database/Models/Lodestone/LodeStoneCache.cs
+++
@@ -1,8 +0,0 @@
-namespace Manabot.Database.Models.Lodestone;
-
-public class LodeStoneCache
-{
-    public DateTime CreatedAt { get; set; }
-    public string? Id { get; set; }
-    public string? Title { get; set; }
-}
